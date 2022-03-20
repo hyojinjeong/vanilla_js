4d13eaad525edb460552605ae32fc511f57f1825
hyojinjeong <82594837+hyojinjeong@users.noreply.github.com>
2022-03-20 21:52:07 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const images = ["nala1.jpg", "nala2.jpg", "nala3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const imgUrl = 'url(' + '../img/' + chosenImage + ')';
+const imgUrl = 'url(' + '../vanilla_js/img/' + chosenImage + ')';
 document.body.style.backgroundImage = imgUrl;
