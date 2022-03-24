3361641b35c5d680bd1afc9cf913e1fe3c81f22b
hyojinjeong <82594837+hyojinjeong@users.noreply.github.com>
2022-03-24 23:52:03 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 const images = ["starry_night1.jpg", "starry_night2.jpg", "starry_night3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const imgUrl = 'url(' + '../vanillaJS/img/' + chosenImage + ')';
+const imgUrl = 'url(' + '../vanilla_js/img/' + chosenImage + ')';
+console.log(imgUrl);
 document.body.style.backgroundImage = imgUrl;
