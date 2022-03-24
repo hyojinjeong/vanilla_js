d7d9ef82c3f7c127e2a35fbb1ba3991efa64a108
hyojinjeong <hyojin.dev@gmail.com>
2022-03-24 23:50:24 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,6 +1,5 @@
-const images = ["nala1.jpg", "nala2.jpg", "nala3.jpg"];
+const images = ["starry_night1.jpg", "starry_night2.jpg", "starry_night3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const imgUrl = 'url(' + '../vanilla_js/img/' + chosenImage + ')';
-console.log(imgUrl);
+const imgUrl = 'url(' + '../vanillaJS/img/' + chosenImage + ')';
 document.body.style.backgroundImage = imgUrl;
